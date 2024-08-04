a1b5a10d15fa5da4e45e9749b10ca41db10f7446
Tú Trần <nhoxcool2002@gmail.com>
2024-08-05 02:48:46 +0700

    PasswordEncoded + SpringSecurity

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -4,6 +4,7 @@ package com.shopme.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Role;
@@ -18,6 +19,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepo;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	
 	public List<User> listAll() {
 		List<User> users = (List<User>) userRepo.findAll();
@@ -30,7 +34,13 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		encodePassword(user);
 		userRepo.save(user);
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 	
 }
